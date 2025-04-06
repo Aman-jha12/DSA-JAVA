9db438dcbe205f1b7afa5fd85dcd305812a987c5
Aman-jha12 <ajha82703@gmail.com>
2025-04-06 23:14:38 +0530

    completed  maxInArray file

--- src/maxInArray.java
+++ src/maxInArray.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class maxInArray {
-}
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of your array :");
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        System.out.println("Now enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int maxArray=max(arr);
+        System.out.println("The maximum in the given array is:" + maxArray);
+    }
+
+    public static int max(int [] arr){
+        int max=0;
+        for(int i=0;i< arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    }
+
