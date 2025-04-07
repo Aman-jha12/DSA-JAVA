196810ea1960990c336c974e6873f2600edfedbc
Aman-jha12 <ajha82703@gmail.com>
2025-04-08 01:11:15 +0530

    max wealth question of Leetcode modifies

--- leetcode/MaxWealth.java
+++ leetcode/MaxWealth.java
@@ -1,31 +1,33 @@
 //leetcode question link=" https://leetcode.com/problems/richest-customer-wealth"
 
 public class MaxWeaalth{
-
-    class Solution {
         public int maximumWealth(int[][] accounts) {
-            int[] wlt = new int[accounts.length];
-            for( int i= 0 ; i<= accounts.length-1; i++){
-                if(accounts[i].length != 1){
-                    for(int j = 1; j<= accounts[i].length-1; j++){
-                        accounts[i][j] = accounts[i][j-1] + accounts[i][j];
-                        wlt[i] = accounts[i][j];
-                    }
+
+            int maxWealth = 0;
+
+            for (int i= 0;i<accounts.length;i++) {
+                int wealth=0;
+
+                for(int j=0; j<accounts[i].length;j++)
+                {
+                    // wealth = wealth + j;
+                    wealth = wealth + accounts[i][j];
                 }
-                else{
-                    wlt[i] = accounts[i][0];
+                if (wealth >= maxWealth){
+                    maxWealth = wealth;
                 }
 
 
             }
-            int max = 0;
-            for(int x = 0; x<= wlt.length -1; x++){
-                if(wlt[x] > wlt[max]){
-                    max = x;
-                }
-            }
-            return wlt[max];
+
+
+
+
+
+
+
+            return  maxWealth;
         }
     }
-    }
+
 
